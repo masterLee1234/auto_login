ded8ba2e5168b398f54efb451ab8e9a25e9511b4
masterLee1234 <47629824+masterLee1234@users.noreply.github.com>
2020-07-15 01:55:10 +0900

    Add files via upload

--- Project10/Project10/main.c
+++ Project10/Project10/main.c
@@ -1,11 +1,35 @@
 #include<stdio.h>
 #include<Windows.h>
-#include"using_cmd.h"
 
 int main() {
-	input_cmd("explorer http://google.com");
-
 	HWND hWndConsole = GetConsoleWindow();
 	ShowWindow(hWndConsole, SW_HIDE);
+
+    RegisterHotKey(NULL, 1, MOD_CONTROL | MOD_ALT | MOD_NOREPEAT, 'B');
+    RegisterHotKey(NULL, 2, MOD_CONTROL | MOD_ALT | MOD_NOREPEAT, 'E');
+
+    MSG msg = { 0 };
+    while (GetMessage(&msg, NULL, 0, 0) != 0)
+    {
+        if (msg.message == WM_HOTKEY)
+        {
+            if (msg.wParam == 1) {
+
+                ShowWindow(hWndConsole, SW_NORMAL);
+                printf("opened");
+                Sleep(500);
+                system("start /max iexplore.exe http://www.ebsi.co.kr");
+                Sleep(500);
+                ShowWindow(hWndConsole, SW_HIDE);
+
+            }
+            else if (msg.wParam == 2) {
+
+                exit(0);
+
+            }
+        }
+    }
+
 	return 0;
 }
